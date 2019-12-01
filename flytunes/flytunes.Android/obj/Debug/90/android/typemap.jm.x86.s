416b286a088780ef24f1d60d4111ea55e1c0ccc2
TshepoMooka <Tshepoxxx@gmail.com>
2019-12-01 13:03:39 +0200

    Initial commit

---
+++ flytunes/flytunes.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: de6d6164cc2b2647ef9e9fd4ed895c1ca695987b */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1284
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
